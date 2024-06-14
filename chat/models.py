13c5e5f5df681b390a957e52e1298db6590caa43
Oluwaseun-ayeni <72kulbranking@gmail.com>
2024-06-13 22:46:00 -0600

    new

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from user.models import User
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Message by {self.user.username} in {self.chat_room.name}"
+
