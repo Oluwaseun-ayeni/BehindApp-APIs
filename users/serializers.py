fc3fc1fcf486c3d3dfd80fd309fa4703ae604cb3
Akintola Damilola Ayodeji <ayodeji2k@gmail.com>
2024-06-17 18:26:47 -0400

    make some changes to the models and serializer. adding the other required fields and use the fields to update the models

--- users/serializers.py
+++ users/serializers.py
@@ -11,7 +11,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'password', 'profile_bio', 'profile_picture', 'ip_address']
+        fields = ['email', 'first_name', 'last_name', 'password', 'profile_bio', 'profile_picture', 'ip_address', 'gender', 'DOB', 'location']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def validate_password(self, value):
         validate_password(value)
@@ -22,26 +25,26 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         picture = validated_data.pop('profile_picture', None)
         ip_address = validated_data.pop('ip_address', '127.0.0.1')
 
-        # Create the user instance with hashed password
         user = User(
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            last_name=validated_data['last_name'],
+            gender=validated_data.get('gender'),
+            DOB=validated_data.get('DOB'),
+            location=validated_data.get('location')
         )
-       
         user.set_password(validated_data['password'])
         user.save()
 
         IPAddress.objects.create(user=user, ip_address=ip_address)
 
-        # Create the profile associated with the user
-        profile = Profile.objects.create(
+        Profile.objects.create(
             user=user,
             bio=bio,
             photo_url=picture.url if picture else None
         )
 
-        return user, profile
+        return user
 
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -63,6 +66,7 @@ class UserLoginSerializer(serializers.Serializer):
             "access": str(refresh.access_token),
             "refresh": str(refresh)
         }
+
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
 
@@ -87,12 +91,13 @@ class ProfileSerializer(serializers.ModelSerializer):
         return profile_instance
 
     def update(self, instance, validated_data):
-        user_data = validated_data.pop('user')
+        user_data = validated_data.pop('user', None)
 
-        instance.user.email = user_data.get('email', instance.user.email)
-        instance.user.first_name = user_data.get('first_name', instance.user.first_name)
-        instance.user.last_name = user_data.get('last_name', instance.user.last_name)
-        instance.user.save()
+        if user_data:
+            instance.user.email = user_data.get('email', instance.user.email)
+            instance.user.first_name = user_data.get('first_name', instance.user.first_name)
+            instance.user.last_name = user_data.get('last_name', instance.user.last_name)
+            instance.user.save()
 
         instance.bio = validated_data.get('bio', instance.bio)
         instance.photo_url = validated_data.get('photo_url', instance.photo_url)
@@ -100,8 +105,3 @@ class ProfileSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
-
-
-
-
-
